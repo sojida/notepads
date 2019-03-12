76348b9c397b77066b14517656e7b6824a40f8e4
Amaechi Chuks <amaechichuks2000@yahoo.com>
2019-03-12 23:44:13 +0100

    [feature]Build out GET REST api endpoint(CRUD) operation

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import Notes from '../controllers/notes';
+import Getone from '../controllers/GetOne';
 
 const router = express.Router();
 
 router.get('/notes', Notes.list);
+
+router.get('/notes/:id', Getone.list);
 
 export default router;
--- server/test/notes.test.js
+++ server/test/notes.test.js
@@ -1,10 +1,30 @@
 /* eslint-disable no-undef */
 import { app, chai, expect } from './index.test';
 
+const url = '/api/v1/notes';
+const id = '7139d3af-b8b4-44f6-a49f-9305791700f4';
+
 describe('GET ALL NOTES', () => {
   it('should respond with all notes', (done) => {
     chai.request(app)
-      .get('/api/v1/notes')
+      .get(url)
+      .end((err, res) => {
+        const { status, data } = res.body;
+        expect(status).to.equal(200);
+        if (data.length) {
+          expect(data[0]).to.have.property('id');
+          expect(data[0]).to.have.property('note');
+          expect(data[0]).to.have.property('tag');
+        }
+        done();
+      });
+  });
+});
+
+describe('GET A SINGLE NOTE', () => {
+  it('should respond with a single notes', (done) => {
+    chai.request(app)
+      .get(`${url}/:${id}`)
       .end((err, res) => {
         const { status, data } = res.body;
         expect(status).to.equal(200);
