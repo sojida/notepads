5142026f516fd1f907d177419a3c0b2f038fbedb
vic3king <andela@MacBook-Pro-7.local>
2019-03-14 23:03:25 +0100

    feature(updatenote): users should be able to update their notes  - create endpoint for updating notes - validate endpoint - write tests for the endpoint

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -10,4 +10,13 @@ router.delete('/notes/:id', middleware.checkId, Notes.deleteNote);
 
 router.post('/notes', middleware.validateNote, Notes.createNote);
 
+router.put('/notes/:id', middleware.checkId, middleware.checksIfIdExists, middleware.checkUpdateFields, Notes.updateNote);
+
+router.all('*', (req, res) => {
+  res.status(404).send({
+    error: {
+      message: 'endpoint does not exist',
+    },
+  });
+});
 export default router;
--- server/test/index.test.js
+++ server/test/index.test.js
@@ -5,6 +5,8 @@ import app from '../src/index';
 
 const deleteid = 'b5f7f2c7-b5b9-47dd-bfea-3372f95404c9';
 const deleteidNotFound = 'b5f7f2c7-b5b9-47dd-bfea-3372f95404c8';
+const updatedId = 'fa3def47-153a-40bd-8181-a1c787e083d6';
+const missingId = '0fc8e96a-d431-4ef4-9fea-b1b8c5cad3f7';
 
 chai.use(chaiHttp);
 
@@ -25,4 +27,6 @@ export {
   app,
   deleteid,
   deleteidNotFound,
+  updatedId,
+  missingId,
 };
