d3c7276856fec5e0b210d0bdf4256f60949705bc
Adesoji Daniel <adesoji.daniel@andela.com>
2019-03-15 11:10:39 +0100

    [Chore]: hosting on heroku

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -8,15 +8,14 @@ const env = process.env.NODE_ENV;
 const config = configSetup[env];
 const db = {};
 
-// For database
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+let sequelize;
+if (config.url) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+// const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs
   .readdirSync(__dirname)
